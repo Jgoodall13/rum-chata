d21359eafec980c9f5b5b50b8f96b4b3adfdcc94
Jacob Goodall <jgoodall@opp.local>
2018-04-09 11:49:01 -0500

    bones created

--- subs/ageCheck/script.js
+++ subs/ageCheck/script.js
@@ -13,6 +13,7 @@ $(document).ready(function(){
             alert('nice, you can drink')
             access = true;
             $('#myModal').css('display', 'none');
+            setCookie('ageVerified', 'true', 1)
         } else {
             alert('sorry bub. too young')
         }
@@ -40,3 +41,37 @@ function getAge(day, month, year) {
     return age;
 }
 
+function setCookie(cname,cvalue,exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires=" + d.toGMTString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
+function checkCookie() {
+    var user=getCookie("ageVerified");
+    if (user != "") {
+        access = true;
+        console.log('we have the right cookie')
+            $('#myModal').css('display', 'none');
+    } else{
+        console.log('We have no cookies')
+    }
+}
